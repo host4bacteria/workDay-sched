ef181c8530a7f8639419e4fd0e360e6070cc3a28
host4bacteria <trishtinaldoctor@yahoo.com>
2023-08-30 02:41:02 -0400

    added current date to app

--- Develop/script.js
+++ Develop/script.js
@@ -9,15 +9,39 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+
+
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
-});
+
+
+
+  // code to display the current date in the header of the page
+
+  var showDate = () => {
+    currentDate = dayjs().format('YYYY-MM-DD HH:mm:ss');
+    console.log(currentDate)
+    $(`#currentDay`).text(currentDate);
+  }
+// updates date/time by second
+  setInterval(showDate, 1000);
+  
+  // function showDate() {
+  //     var currentDate = dayjs().format('YYYY-MM-DD HH:mm:ss');
+  //     $(`#currentDay`).text(currentDate);
+  //   }
+    
+  });
